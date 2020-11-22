04e81103409d1fe52fe322460a0308df012bbc5f
Adrian Albert Ciubucciu <ciubucciu.adrianalbert@gmail.com>
2020-11-22 13:52:25 +0200

    Updates

---
+++ precache-manifest.536aac24e57990930423d2ecaabae433.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6aa93c674191dec64a51",
+    "url": "/React-Interface/static/css/main.98629454.chunk.css"
+  },
+  {
+    "revision": "6aa93c674191dec64a51",
+    "url": "/React-Interface/static/js/main.6aa93c67.chunk.js"
+  },
+  {
+    "revision": "8fe5b8637ed198ccf4f7",
+    "url": "/React-Interface/static/css/1.d9d1df30.chunk.css"
+  },
+  {
+    "revision": "8fe5b8637ed198ccf4f7",
+    "url": "/React-Interface/static/js/1.8fe5b863.chunk.js"
+  },
+  {
+    "revision": "ef95919d4ea0a98b4469",
+    "url": "/React-Interface/static/js/runtime~main.ef95919d.js"
+  },
+  {
+    "revision": "37e8e353e578dac2b8ad0b02b5a415bf",
+    "url": "/React-Interface/index.html"
+  }
+];
